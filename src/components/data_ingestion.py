303e6b5a28e34c2fcdf915bcdce37d668be571fe
Purushothaman <haripurushoth185@gmail.com>
2024-05-26 10:42:34 +0530

    Model Training Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationconfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 ## this dataclass used to intialize the class variable inside class without using init directly...
@@ -54,4 +56,13 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    try:
+        modeltrainer = ModelTrainer()
+        print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+
